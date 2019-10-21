6055cf5160704faac73c316cd86c2334108bba97
alexalins <alexa.glinsoo@gmail.com>
2019-10-21 19:57:41 -0300

    token

--- src/app/shared/services/auth.service.ts
+++ src/app/shared/services/auth.service.ts
@@ -1,5 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { tap } from 'rxjs/operators';
+import { TokenService } from './token.service';
 
 const API_URL = 'http://localhost:3000';
 @Injectable({
@@ -7,9 +9,17 @@ const API_URL = 'http://localhost:3000';
 })
 export class AuthService {
 
-  constructor(private http: HttpClient) { }
+  constructor(
+    private http: HttpClient,
+    private tokenService: TokenService
+  ) { }
 
   authenticate(userName: string, password: string) {
-    return this.http.post(API_URL + '/user/login', { userName, password })
+    return this.http.post(API_URL + '/user/login', { userName, password }, { observe: 'response' })
+      .pipe(tap(res => {
+        const authToken = res.headers.get('x-access-token');
+        this.tokenService.setToken(authToken);
+        console.log(`User ${userName} authenticated with token ${authToken}`);
+      }))
   }
 }
